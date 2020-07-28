e92ed596034b187943dbc7a946b36b425502401e
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2020-07-29 06:44:03 +0700

    add pages to guide compoennts

---
+++ pages/components/alerts.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h3 class="text-gray-700 text-xl font-medium">Alerts</h3>
+
+    <div class="mt-4">
+      <div class="mt-4">
+        <h4 class="my-2">
+          <a href="#alert-default"
+            ><SvgLink class="w-5 h-5 inline-block text-indigo-800"
+          /></a>
+          Alert Default
+        </h4>
+        <div
+          class="rounded-md bg-white py-4 px-4 overflow-x-auto whitespace-no-wrap"
+        >
+          <Alert class="ml-3">This is an alert.</Alert>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
